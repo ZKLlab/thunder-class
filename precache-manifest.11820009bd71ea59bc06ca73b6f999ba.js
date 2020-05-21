0893e12ca1fd5134ebad19e2da73a31106b4a88d
钟坤林 <zkl@zkllab.com>
2020-05-21 23:06:36 +0800

    Updates

---
+++ precache-manifest.11820009bd71ea59bc06ca73b6f999ba.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "467da3d4c54b54b6027e",
+    "url": "/css/app.650e30f5.css"
+  },
+  {
+    "revision": "22350e3e28d82ee057e3",
+    "url": "/css/chunk-vendors.4b6f6ee1.css"
+  },
+  {
+    "revision": "541e65fb46e44ecdf7a9da8a9b881446",
+    "url": "/fonts/materialdesignicons-webfont.541e65fb.eot"
+  },
+  {
+    "revision": "c61b9c12f68ee1ba045a4b49dba29ca5",
+    "url": "/fonts/materialdesignicons-webfont.c61b9c12.woff2"
+  },
+  {
+    "revision": "fc03f7f15facede623faa7666c7d1f5a",
+    "url": "/fonts/materialdesignicons-webfont.fc03f7f1.ttf"
+  },
+  {
+    "revision": "ff13d121c1a1cf74d8e06bd39e1746c3",
+    "url": "/fonts/materialdesignicons-webfont.ff13d121.woff"
+  },
+  {
+    "revision": "ed4216b2c075d6c4f2363f854ca8adb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "467da3d4c54b54b6027e",
+    "url": "/js/app.06cf5e34.js"
+  },
+  {
+    "revision": "22350e3e28d82ee057e3",
+    "url": "/js/chunk-vendors.febafa67.js"
+  },
+  {
+    "revision": "f2cc8f32ce48ce56d0ee57c5706fc645",
+    "url": "/lib/L2Dwidget.0.min.js"
+  },
+  {
+    "revision": "64a9f168901f7068f9b9052c0e245a45",
+    "url": "/lib/L2Dwidget.common.js"
+  },
+  {
+    "revision": "9cf043017b9fbe14a8fa8cfc88f2a625",
+    "url": "/lib/L2Dwidget.min.js"
+  },
+  {
+    "revision": "f32a08cd21fd2a66f47dbeee60c01a75",
+    "url": "/live2d.html"
+  },
+  {
+    "revision": "11cdb41b70fcf1e7ca1c18e0e7f954c1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "47047fee26557b55d985952bdfc6cba1",
+    "url": "/models/face_landmark_68_tiny_model-shard1"
+  },
+  {
+    "revision": "29ea9c5c0e59a3069f8999b4ba1bd173",
+    "url": "/models/face_landmark_68_tiny_model-weights_manifest.json"
+  },
+  {
+    "revision": "2e48b20953b0c59df47459d0319843a0",
+    "url": "/models/tiny_face_detector_model-shard1"
+  },
+  {
+    "revision": "5bab50532388f5da9b4cd85b15adc11c",
+    "url": "/models/tiny_face_detector_model-weights_manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
